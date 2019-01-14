cab721094c04d4eb18fb4de1728bea65c865c7a4
wry5560 <233089043@qq.com>
2019-01-14 09:45:50 +0800

    增加ie兼容配置

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import 'ant-design-vue/dist/antd.less'  // or 'ant-design-vue/dist/antd.less'
 import '@/permissionAjj' // ajj permission control
 import '@/utils/filter' // base filter
 import PermissionHelper from '@/utils/helper/permission'
+import '@babel/polyfill'
 
 import {
   ACCESS_TOKEN,
